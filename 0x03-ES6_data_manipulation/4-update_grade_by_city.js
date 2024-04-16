60d6627c5de41eb76fa5ead369e7704b6c47424d
nobleenia <neluwah@gmail.com>
2024-04-16 16:44:43 +0000

    Corrections

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -5,7 +5,7 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   if (!Array.isArray(newGrades)) {
     return [];
   }
-    return students;
+    return students
     .filter((student) => student.location === city)
     .map((student) => {
       const [newGrade] = newGrades.filter((item) => item.studentId === student.id);
