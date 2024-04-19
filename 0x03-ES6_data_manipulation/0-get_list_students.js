e141c8a62a2b33b38c2a58875dd99033c4e17518
nobleenia <neluwah@gmail.com>
2024-04-19 21:07:51 +0000

    Update: Corrections

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
-export default function getListStudents() {
-  return [
+const getListStudents = () => [
     { id: 1, firstname: 'Guillaume', lastname: 'San Francisco' },
     { id: 2, firstname: 'James', lastname: 'Columbia' },
     { id: 5, firstname: 'Serena', lastname: 'San Francisco' },
-  ];
-}
+];
+
+export default getListStudents;
